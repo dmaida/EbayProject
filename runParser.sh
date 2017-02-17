9e54a7935632c50caa98f9832b9333abc7d41ba5
Steven Warren Call <steven.call@wsu.edu>
2017-02-16 16:06:47 -0800

    fixed parser and duplicate issue. Working on queries.

--- runParser.sh
+++ runParser.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-python skeleton_parser.py ./ebay_data/items-*.xml 
+python fileParser.py ./ebay_data/items-*.xml 
 
 sort user.dat | uniq > user.out
 mv -f user.out user.dat
 
 sqlite3 ebayD.db < create.sql
+
+sqlite3 ebayD.db < query1.sql
+sqlite3 ebayD.db < query2.sql
+sqlite3 ebayD.db < query3.sql
+sqlite3 ebayD.db < query4.sql
+sqlite3 ebayD.db < query5.sql
+sqlite3 ebayD.db < query6.sql
+sqlite3 ebayD.db < query7.sql
