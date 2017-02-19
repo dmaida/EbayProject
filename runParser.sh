881c0bcf5149ff08105aa2e712c804b0530e96bb
Steven Warren Call <steven.call@wsu.edu>
2017-02-18 19:06:53 -0800

    Finalized project

--- runParser.sh
+++ runParser.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-python fileParser.py ebay_data/items-*.xml
+#use on personal machine on data
+#python fileParser.py ebay_data/items-*.xml 
 
-#python fileParser.py /usr/class/cs351/project/ebay_data/items-*.xml
+python fileParser.py /usr/class/cs351/project/ebay_data/items-*.xml
 
 sort user.dat | uniq > user.out
 mv -f user.out user.dat
 
 sqlite3 ebayD.db < create.sql
 sqlite3 ebayD.db < load.txt
+
+sqlite3 ebayD.db < query1.sql
+sqlite3 ebayD.db < query2.sql
+sqlite3 ebayD.db < query3.sql
+sqlite3 ebayD.db < query4.sql
+sqlite3 ebayD.db < query5.sql
+sqlite3 ebayD.db < query6.sql
+sqlite3 ebayD.db < query7.sql
