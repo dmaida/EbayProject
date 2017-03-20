e99e5327dc81e8f9fb3930d550c5464986a1033a
Nazar Stelmakh (Ubuntu) <nazar.stelmakh@wsu.edu>
2017-03-20 12:21:33 -0700

    Added price constraint trigger4. Still needs testing.

---
+++ trigger4_add.sql
@@ -0,0 +1,12 @@
+-- Any new bid for a particular item must have a higher amount than any of the previous bids for that particular item. (TRIGGER)
+
+PRAGMA foreign_keys = ON;
+
+drop TRIGGER if exists price_constraint;
+create TRIGGER price_constraint
+after INSERT on bid
+for each row
+when New.Amount <= (select currently from Item where Item.ItemID = New.ItemID)
+begin
+select raise(rollback, 'Bid amount must be greater than the current bid.');
+end;
