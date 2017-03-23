be2dd85f64d38254b9e5fd6a994091d27a530fe6
Daniel Esteban Maida <daniel.maida@wsu.edu>
2017-03-23 12:24:19 -0700

    Changed the runParser to use the file path in the argo server. Should be ready to be turned in.

--- test.sql
+++
@@ -1,3 +0,0 @@
-insert into Bid values ('1049482147','Bitch','2001-12-20 00:00:03','6.02');
---update CurrentTime set currtime = '2001-12-18 18:49:57';
---  select count(*) from Items where '2005-12-17 12:45:57' between Starting AND Ending AND ItemID = 1049482147;
