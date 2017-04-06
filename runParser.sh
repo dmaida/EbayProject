ac43b908f938ab99c45482848cf7bdb2db50e899
Daniel Esteban Maida <daniel.maida@wsu.edu>
2017-04-06 13:28:52 -0700

    Created the database.

--- runParser.sh
+++ runParser.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #use on personal machine on data
-#python fileParser.py ebay_data/items-*.xml
+python fileParser.py ebay_data/items-*.xml
 
-python fileParser.py /usr/class/cs351/project/ebay_data/items-*.xml
+#python fileParser.py /usr/class/cs351/project/ebay_data/items-*.xml
 
 sort user.dat | uniq > user.out
 mv -f user.out user.dat
